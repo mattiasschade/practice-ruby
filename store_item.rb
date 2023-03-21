486c16824b0f4839f12fa49f3d263bebea4a62df
Mattias Schade <mattiasschade@gmail.com>
2023-03-21 14:29:33 -0500

    added more hashes, print pineapple info

--- store_item.rb
+++ store_item.rb
@@ -4,3 +4,12 @@ pear = {name: "Pear", price: 3.00, size: "large"}
 
 puts "The cost of one #{apple[:size]} #{apple[:name]} is $#{apple[:price]}."
 
+
+pineapple = {:name => "Pineapple", :price => 4.50, :size => "large"}
+cucumber = {:name => "Cucumber", :price => 3.50, :size => "small"}
+bread = {:name => "Bread", :price => 4.00, :size => "large"}
+
+puts "The cost of one #{pineapple[:size]} #{pineapple[:name]} is $#{pineapple[:price]}."
+
+
+
