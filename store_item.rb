9a0dca2feb80f3dcbe2d489bc14bfcc079202689
Mattias Schade <mattiasschade@gmail.com>
2023-03-21 14:23:12 -0500

    added store items as hashes

---
+++ store_item.rb
@@ -0,0 +1,6 @@
+apple = {name: "Apple", price: 2.25, size: "large"}
+avocado = {name: "Avocado", price: 4.50, size: "small"}
+pear = {name: "Pear", price: 3.00, size: "large"}
+
+puts "The cost of one #{apple[:size]} #{apple[:name]} is $#{apple[:price]}."
+
