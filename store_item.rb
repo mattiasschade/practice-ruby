a75f8941ed6b3498eef9c8da1d781bb21dff3a79
Mattias Schade <mattiasschade@gmail.com>
2023-03-22 11:48:29 -0500

    changed puts to p

--- store_item.rb
+++ store_item.rb
@@ -2,14 +2,15 @@ apple = {name: "Apple", price: 2.25, size: "large"}
 avocado = {name: "Avocado", price: 4.50, size: "small"}
 pear = {name: "Pear", price: 3.00, size: "large"}
 
-puts "The cost of one #{apple[:size]} #{apple[:name]} is $#{apple[:price]}."
+p "The cost of one #{apple[:size]} #{apple[:name]} is $#{apple[:price]}."
 
 
 pineapple = {:name => "Pineapple", :price => 4.50, :size => "large"}
 cucumber = {:name => "Cucumber", :price => 3.50, :size => "small"}
 bread = {:name => "Bread", :price => 4.00, :size => "large"}
 
-puts "The cost of one #{pineapple[:size]} #{pineapple[:name]} is $#{pineapple[:price]}."
+p "The cost of one #{pineapple[:size]} #{pineapple[:name]} is $#{pineapple[:price]}."
+
 
 
 
