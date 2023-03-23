bdd7f82a13fec3ab0de1a7864c2bd43a3b418c9d
Mattias Schade <mattiasschade@gmail.com>
2023-03-23 12:38:26 -0500

    added fire_all_employees method, added give_all_raises method

--- manager.rb
+++ manager.rb
@@ -34,8 +34,30 @@ class Manager < Employee
     # use email sending library...
     puts "Email sent!"
   end
+
+  def give_all_raises
+    index = 0
+    @employees.length.times do
+      @employees[index].give_annual_raise
+      index += 1
+    end
+  end
+
+  def fire_all_employees
+    index = 0
+    @employees.length.times do
+      @employees[index].active = false
+      index += 1
+    end
+  end
 end
 
 manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true, employees: [employee1, employee2])
 manager.print_info
 manager.send_report
+manager.give_all_raises
+employee1.print_info
+employee2.print_info
+manager.fire_all_employees
+p employee1
+p employee2
