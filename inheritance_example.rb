4239a4b6dd2199623b6f811f9a481411e05f9648
Mattias Schade <mattiasschade@gmail.com>
2023-03-23 11:45:14 -0500

    inherited from vehicle

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,4 +1,4 @@
-class Car
+class Vehicle
   def initialize
     @speed = 0
     @direction = 'north'
@@ -15,30 +15,15 @@ class Car
   def turn(new_direction)
     @direction = new_direction
   end
+end
 
+class Car < Vehicle
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
-class Bike < Car
-  # def initialize
-  #   @speed = 0
-  #   @direction = 'north'
-  # end
-
-  # def brake
-  #   @speed = 0
-  # end
-
-  # def accelerate
-  #   @speed += 10
-  # end
-
-  # def turn(new_direction)
-  #   @direction = new_direction
-  # end
-
+class Bike < Vehicle
   def ring_bell
     puts "Ring ring!"
   end
@@ -50,3 +35,6 @@ p bike1
 car1 = Car.new
 p car1.honk_horn
 p bike1.ring_bell
+
+p car1.turn("left")
+p bike1.accelerate
